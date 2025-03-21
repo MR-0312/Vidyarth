a6bc8edb51e7502a60c50c981f7b82b0e66285ca
ishansavaliya <ishansavaliya787@gmail.com>
2025-03-21 14:42:19 +0530

    Update server port to 8080 and fix console log message. Remove Login and Signup components from frontend.

--- backend/server.js
+++ backend/server.js
@@ -33,6 +33,6 @@ app.use((err, req, res, next) => {
   res.status(500).send("Something went wrong!");
 });
 
-const PORT = process.env.PORT || 5002;
+const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log("Server running on port ${PORT}"));
